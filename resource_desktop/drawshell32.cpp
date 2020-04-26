b6ef69c21b791fa95ef70645184b200f335ae40a
bacz <57490121+bacz666@users.noreply.github.com>
2020-04-26 21:08:32 +0800

    Add files via upload

--- resource_desktop/drawshell32.cpp
+++ resource_desktop/drawshell32.cpp
@@ -1,24 +1,34 @@
 #define _WIN32_WINNT 0x601
 #include <windows.h>
+//#include <iostream>
+//using namespace std;
 POINT p;
 HDC hdc;
-BOOL CALLBACK EnumResNameProc(HMODULE hModule, LPCTSTR lpszType, LPTSTR lpszName, LPARAM lParam){
-	if(lpszType == RT_GROUP_ICON){
-		GetCursorPos(&p);
-		HICON hIcon = LoadIcon(hModule, lpszName);
-		int ix = GetSystemMetrics(SM_CXICON) / 2, iy = GetSystemMetrics(SM_CYICON) / 2;
-		DrawIcon(hdc, p.x-ix, p.y-iy, LoadIcon(hModule, lpszName));
-	}
-	Sleep(20);
+int x0, y0, x1, y1, x2, y2;
+
+BOOL CALLBACK EnumResNameProc(HMODULE hModule, LPCTSTR lpszType, LPTSTR lpszName, LPARAM lParam) {
+	GetCursorPos(&p);
+	HICON hIcon = LoadIcon(hModule, lpszName);
+	x2 = int(double(p.x) / x1 * x0), y2 = int(double(p.y) / y1 * y0);
+	int ix = GetSystemMetrics(SM_CXICON) / 2, iy = GetSystemMetrics(SM_CYICON) / 2;
+
+	DrawIcon(hdc, x2 - ix, y2 - iy, LoadIcon(hModule, lpszName));
+//	cout << x0 << "\t" << y0 << "\t" << x1 << "\t" << y1 << "\t" << x2 << "\t" << y2 << "\t" << p.x << "\t" << p.y << "\n";
+	Sleep(10);
 	return TRUE;
 }
 
-int main(){
+int main() {
 	hdc = GetDC(NULL);
 	HMODULE hShell32 = LoadLibrary("shell32.dll");
-	int w = GetSystemMetrics(SM_CXSCREEN), h = GetSystemMetrics(SM_CYSCREEN);
-	for(;;){
+	x1 = GetSystemMetrics(SM_CXSCREEN), y1 = GetSystemMetrics(SM_CYSCREEN);
+	HDC hdc1 = GetDC(NULL);
+	int x0 = GetDeviceCaps(hdc1, DESKTOPHORZRES);
+	int y0 = GetDeviceCaps(hdc1, DESKTOPVERTRES);
+	ReleaseDC(NULL, hdc1);
+	system("pause");
+	for (;;) {
 		EnumResourceNamesA(hShell32, RT_GROUP_ICON, EnumResNameProc, 0);
-		Sleep(20);
+		Sleep(1);
 	}
 }
